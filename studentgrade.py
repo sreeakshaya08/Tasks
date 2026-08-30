b0f20fd95a284b79e6089396e3eed13151e2bfba
Sree Akshaya <sreeakshayakotwalguda@gmail.com>
2026-08-30 21:36:17 +0530

    Add files via upload

---
+++ studentgrade.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter your marks: "))
+if marks == 0 or marks > 100:
+    print("Invalid marks!")
+elif marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 60:
+    print("Grade: C")
+elif marks >= 40:
+    print("Grade: D")
+else:
+    print("Fail")
+    
